001884adb24124d028d80d0e993217c4d9227dfe
Gustavo Carvalho <gustavo.carvalhoalves.santos@gmail.com>
2024-03-15 17:26:30 -0300

    @GustavoCarvalho25 - Cria API de Usuários e estrutura básica de 1 modelo

---
+++ UsuariosAPI/UsuariosAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Data.Dtos;
+
+namespace UsuariosAPI.Controllers
+{
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario
+        (
+            CreateUsuarioDto createUsuarioDto
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
